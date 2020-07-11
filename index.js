37533bf53c034b9d5b8582fb78a1c34390871b8a
Vivekanandan GM <joker12345.rm@gmail.com>
2020-07-11 19:17:01 +0530

    customer features

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded',function(){
-    var elems = document.querySelectorAll('.slider');
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems,{});
+});
+
+document.addEventListener('DOMContentLoaded',function(){
+    var elems = document.querySelectorAll('.sidenav');
     var instances = M.Slider.init(elems,{});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {dist:-100});
 });
