f96e26afe228c810ef0fa5d015df25186547cafa
Vivekanandan GM <joker12345.rm@gmail.com>
2020-07-12 16:56:49 +0530

    maps

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {dist:-100});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {});
+});
